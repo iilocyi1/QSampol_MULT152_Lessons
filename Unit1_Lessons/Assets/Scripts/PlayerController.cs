aa5a99b6d56be87f45326422fe52fa243759794a
iilocyi1 <124301687+iilocyi1@users.noreply.github.com>
2024-08-27 23:48:35 -0700

    Unit1_Lesson3D

--- Unit1_Lessons/Assets/Scripts/PlayerController.cs
+++ Unit1_Lessons/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15.0f;
+    //rate of speed, turn speed does not correspond with value
+    private float speed = 15.0f;
+    private float turnSpeed = 1.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +19,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         Debug.Log(Time.deltaTime);
         //Vector3.forward -- > (0,0,1)
         // (0,0,1) * Time.deltaTime = (0, 0, .004)
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);   
-            }
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //removed Time.deltaTime from rotate to speed up turn
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput);
+     }
 }
