174f8b9a63efca2367bcb408590a906ac3509a98
iilocyi1 <124301687+iilocyi1@users.noreply.github.com>
2024-10-14 23:45:52 -0700

    QSampol_Unit3LessonD

--- QSampol_Unit3/Assets/Scripts/PlayerController.cs
+++ QSampol_Unit3/Assets/Scripts/PlayerController.cs
@@ -13,11 +13,19 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
 
     private Animator aniPlayer;
+    public ParticleSystem expSystem;
+    public ParticleSystem dirtSystem;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource asPlayer;
+
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         aniPlayer = GetComponent<Animator>();
+
+        asPlayer = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,6 +38,9 @@ public class PlayerController : MonoBehaviour
             rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onGround = false;
             aniPlayer.SetTrigger("Jump_trig");
+            dirtSystem.Stop();
+            asPlayer.PlayOneShot(jumpSound, 4.0f);
+
         }
     }
 
@@ -38,6 +49,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             onGround = true;
+            dirtSystem.Play();
         }
 
         else if (collision.gameObject.CompareTag("Obstacle"))
@@ -46,6 +58,9 @@ public class PlayerController : MonoBehaviour
             gameOver = true;
             aniPlayer.SetBool("Death_b", true);
             aniPlayer.SetInteger("DeathType_int", 2);
+            expSystem.Play();
+            dirtSystem.Stop();
+            asPlayer.PlayOneShot(crashSound, 4.0f);
 
         }
     }
